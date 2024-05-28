1238026be05abbee0508ab91422b0bdf23081ccc
Dog-Web <11raavan@gmail.com>
2024-05-28 10:54:30 +0530

    number changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const IndexPage: React.FC<PageProps> = () => {
       <Navbar ></Navbar>
       <Gallery/>
       <a
-        href={`https://wa.me/9785189532`}
+        href={`https://wa.me/+919785189532`}
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-colors"
         target="_blank"
         rel="noopener noreferrer"
